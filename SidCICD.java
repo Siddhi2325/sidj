9f5fa93fcd3c5d08ee62223f9c3af0834c528e5f
Siddhi2325 <siddhihon25@gmail.com>
2024-12-30 13:12:04 +0530

    Add files via upload

---
+++ SidCICD.java
@@ -0,0 +1,9 @@
+class SidCICD {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b; // Declare 'sum' before using it
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+
